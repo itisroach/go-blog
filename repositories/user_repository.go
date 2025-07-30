ca4b083fa2359d5fe6bd9e6a0d55f24d7ab8a041
unknown <rouchashoori@gmail.com>
2025-07-31 00:20:10 +0330

    Add function for creating user in database and fetching a user by username from database

---
+++ repositories/user_repository.go
@@ -0,0 +1,31 @@
+package repositories
+
+import (
+	"github.com/itisroach/go-blog/database"
+	"github.com/itisroach/go-blog/models"
+)
+
+func CreateUser(user *models.User) error {
+	
+	result := database.DB.Create(user)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+
+func GetUser(username string) (*models.UserResponse, error) {
+	
+	var user *models.User
+
+	result := database.DB.First(&user, "username = ?", username) 
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return user.NewUserResponse(), nil
+}
