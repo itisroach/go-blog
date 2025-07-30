f4ed21d378dcbde13f7f652d4cd3c6f3acdb59cb
unknown <rouchashoori@gmail.com>
2025-07-31 00:21:27 +0330

    Add a method for user struct to hash password and replace with text like password and another struct to show user response with user struct but only fields that are necessary

--- models/user_model.go
+++ models/user_model.go
@@ -1,10 +1,47 @@
 package models
 
+import (
+	"fmt"
+	"time"
 
+	"github.com/itisroach/go-blog/utils"
+	"gorm.io/gorm"
+)
 
 type User struct {
-	ID 			int		`gorm:"primarykey"`
-	Username	string	`gorm:"uniqueindex:idx_username"`
-	Name		string
-	Password 	string	
+	gorm.Model
+	ID       int    `gorm:"primarykey"`
+	Username string `gorm:"uniqueindex:idx_username" binding:"required,min=4,max=20"`
+	Name     string	`gorm:"default:Unknown"`
+	Password string `gorm:"<-:create" binding:"required,min=8,max=64"`
+}
+
+
+type UserResponse struct {
+	Id 			int
+	Username	string
+	Name 		string
+	CreatedAt 	time.Time
+	UpdatedAt 	time.Time
+}
+
+
+func (u *User) NewUserResponse() *UserResponse{
+	return &UserResponse{
+		Id:       u.ID,
+		Username: u.Username,
+		Name:     u.Name,
+	}
+}
+
+
+func (u *User) HashPassword() error {
+	var err error
+	u.Password, err = utils.HashString(u.Password)
+
+	if err != nil {
+		return err
+	}
+	fmt.Println(*u)
+	return nil
 }
