a3b74ebb046d42275ae85a58d556ddfef9698405
unknown <rouchashoori@gmail.com>
2025-07-31 00:18:45 +0330

    Add error handling for bad request or missing fields the errors are user friendly

---
+++ utils/error.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func FormatError(fieldError validator.FieldError) string {
+	field := strings.ToLower(fieldError.Field())
+
+	switch fieldError.Tag() {
+	case "required":
+		return field + " is required"
+	case "min":
+		return field + " must be at least " + fieldError.Param() + " characters"
+	case "max":
+		return field + " must be at most " + fieldError.Param() + " characters"
+	}
+
+	return field + " is invalid"
+}
