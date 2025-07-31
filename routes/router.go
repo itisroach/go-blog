5e7aef5a4dbf96475429bcc4fedaeb5d2be0b529
unknown <rouchashoori@gmail.com>
2025-07-31 22:18:02 +0330

    Add protected routes using custom auth middleware

--- routes/router.go
+++ routes/router.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/itisroach/go-blog/controllers"
+	"github.com/itisroach/go-blog/middlewares"
 )
 
 func SetupRouter() *gin.Engine {
@@ -12,7 +13,12 @@ func SetupRouter() *gin.Engine {
 	api := router.Group("/api")
 	{
 		api.POST("/auth/register", controllers.RegisterUser)
-		api.POST("/auth/login", controllers.LoginUser)
+ 		api.POST("/auth/login", controllers.LoginUser)
+		
+		protected := api.Group("/")
+		protected.Use(middlewares.AuthMiddleware())
+		
+		protected.POST("/auth/refresh", controllers.RefreshToken)
 	}
 
 	return router
