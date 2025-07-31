1e22bca86167b7e11d70b2f085a39611e6f429e8
unknown <rouchashoori@gmail.com>
2025-07-31 22:16:26 +0330

    Add function to verify tokens

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,14 +1,20 @@
 package utils
 
 import (
+	"errors"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/itisroach/go-blog/models"
+	"github.com/itisroach/go-blog/repositories"
 )
 
+var secret = []byte(os.Getenv("JWT_SECRET"))
+
 func GenerateJWTToken(username string, isRefresh bool) (string, error) {
-	secret := []byte(os.Getenv("JWT_SECRET"))
+	
 
 	var claim jwt.MapClaims
 
@@ -36,5 +42,75 @@ func GenerateJWTToken(username string, isRefresh bool) (string, error) {
 		return "", err
 	}
 
+	if isRefresh{
+		repositories.SaveTokenToDB(tokenString, claim["username"].(string), time.Unix(claim["exp"].(int64), 0))
+	}
+	
 	return tokenString, nil
+}
+
+
+
+func VerifyToken(token string) (*models.TokenClaims, *CustomError) {
+
+	tokenParsed, err := jwt.ParseWithClaims(token, &jwt.MapClaims{}, func(t *jwt.Token) (interface{}, error) {
+		
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return secret, nil
+	})
+
+
+	if err != nil {
+		return nil, &CustomError{
+			Code: http.StatusInternalServerError,
+			Message: "error while parsing token",
+		}
+	}
+
+
+	if !tokenParsed.Valid {
+		return nil, &CustomError{
+			Code: http.StatusBadRequest,
+			Message: "invalid token",
+		}
+	}
+
+	claims, ok := tokenParsed.Claims.(*jwt.MapClaims)
+
+
+	if !ok {
+		return nil, &CustomError{
+			Code: http.StatusBadRequest,
+			Message: "invalid token claims",
+		}
+	}
+	username, _ := (*claims)["username"].(string)
+	tokenType, _ := (*claims)["type"].(string)
+	expFloat, ok := (*claims)["exp"].(float64)
+
+	if !ok {
+		return nil, &CustomError{
+			Code:    http.StatusBadRequest,
+			Message: "invalid expiration claim",
+		}
+	}
+
+	expTime := time.Unix(int64(expFloat), 0)
+
+	if time.Now().After(expTime) {
+		return nil, &CustomError{
+			Code:    http.StatusUnauthorized,
+			Message: "token has expired",
+		}
+	}
+
+
+	return &models.TokenClaims{
+		Username: username,
+		Type:     tokenType,
+		ExpTime:  expTime,
+	}, nil
+
 }
