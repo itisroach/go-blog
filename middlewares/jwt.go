cdde427b0229d2ee130508d136e6600334bfba93
unknown <rouchashoori@gmail.com>
2025-07-31 22:20:28 +0330

    Create a custom middleware for verifying jwt access tokens

---
+++ middlewares/jwt.go
@@ -0,0 +1,35 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/itisroach/go-blog/utils"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("authorization")
+
+		if !strings.HasPrefix(authHeader,"Bearer ") {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			ctx.Abort()
+			return
+		}
+
+		token := strings.TrimPrefix(authHeader, "Bearer ")
+
+		claims, err := utils.VerifyToken(token)
+
+		if err != nil {
+			ctx.JSON(err.Code, gin.H{"error": err.Message})
+			ctx.Abort()
+			return
+		}
+
+		ctx.Set("user", claims.Username)
+
+		ctx.Next()
+	}
+}
