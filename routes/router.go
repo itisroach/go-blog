d7ab84585c9be2c621c69ad8c1a6154bde258799
unknown <rouchashoori@gmail.com>
2025-07-31 14:39:26 +0330

    Add login route

--- routes/router.go
+++ routes/router.go
@@ -9,10 +9,10 @@ func SetupRouter() *gin.Engine {
 
 	router := gin.Default()
 
-
 	api := router.Group("/api")
 	{
 		api.POST("/auth/register", controllers.RegisterUser)
+		api.POST("/auth/login", controllers.LoginUser)
 	}
 
 	return router
