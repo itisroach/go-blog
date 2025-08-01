aeeafe3ed0c9d7c8e6d9b61f808039672f423a1c
unknown <rouchashoori@gmail.com>
2025-08-01 22:33:40 +0330

    Fix route bugs with route parameter

--- routes/router.go
+++ routes/router.go
@@ -16,7 +16,7 @@ func SetupRouter() *gin.Engine {
  		api.POST("/auth/login", controllers.LoginUser)
 		
 		api.GET("/posts", controllers.GetPosts)
-		api.GET("/posts/:id", controllers.GetPost)
+		
 		
 		protected := api.Group("/")
 		protected.Use(middlewares.AuthMiddleware())
@@ -28,6 +28,8 @@ func SetupRouter() *gin.Engine {
 		protected.POST("/posts/write", controllers.NewPost)
 
 		protected.GET("/users/posts/:username", controllers.GetUsersPost)
+
+		api.GET("/posts/:id", controllers.GetPost)
 	}
 
 	return router
