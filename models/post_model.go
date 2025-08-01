86fa2b98284d08d914e3421f9182ff5b2e3a8256
unknown <rouchashoori@gmail.com>
2025-08-01 18:03:19 +0330

    Create models for posts

---
+++ models/post_model.go
@@ -0,0 +1,53 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Post struct {
+	gorm.Model
+	UserID	uint 
+	User  	User 		`gorm:"OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Title 	string
+	Body  	string
+}
+
+
+type PostRequest struct {
+	Username 	string
+	Title		string	`binding:"required,min=8,max=128"`
+	Body		string	`binding:"required,min=24"`
+}
+
+
+type PostResponse struct {
+	ID 			uint
+	User 		*UserResponse
+	Title 		string
+	Body		string
+	CreatedAt	time.Time
+	UpdatedAt	time.Time
+}
+
+
+func MakePostResponse(post *Post) *PostResponse {
+	return &PostResponse{
+		ID: post.ID,
+		User: NewUserResponse(&post.User),
+		Title: post.Title,
+		Body: post.Body,
+		CreatedAt: post.CreatedAt,
+		UpdatedAt: post.UpdatedAt,
+	}
+}
+
+func (p *PostRequest) MakePost(user *User) *Post {
+	return &Post{
+		UserID: uint(user.ID),
+		User: *user,
+		Title: p.Title,
+		Body: p.Body,
+	}
+}
