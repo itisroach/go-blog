3461ec7d0f99d0a5dc04690c4f90dc695fd5907d
unknown <rouchashoori@gmail.com>
2025-08-01 18:04:03 +0330

    Write a controller for post creation

---
+++ controllers/post_controller.go
@@ -0,0 +1,71 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/itisroach/go-blog/models"
+	"github.com/itisroach/go-blog/services"
+	"github.com/itisroach/go-blog/utils"
+)
+
+// NewPost godoc
+// @Summary      Write a new post
+// @Description  Write a new post with title and body
+// @Tags         Post
+// @Accept       json
+// @Produce      json
+// @Param        post  body      models.PostRequest  true  "Post data"
+// @Success      201   {object}  models.PostResponse
+// @Failure      400   {object}  map[string]interface{}
+// @Failure      404   {object}  map[string]interface{}
+// @Router       /posts/write [post]
+func NewPost(c *gin.Context) {
+
+	reqBody := &models.PostRequest{} 
+
+	username, ok := c.Get("user")
+
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "authorization header missed, make sure you have a valid token in headers",
+		})
+		return
+	}
+
+	reqBody.Username = username.(string)
+
+	if err := c.ShouldBindJSON(&reqBody); err != nil {
+		
+		allErrors := utils.GenerateUserFriendlyError(err)
+
+
+		if allErrors == nil{
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": allErrors,
+		})
+		return
+	}
+
+	result, err := services.CreatePostService(reqBody)
+
+
+	if err != nil {
+		c.JSON(err.Code, gin.H{
+			"error": err.Message,
+		})
+		return
+	}
+
+
+	post := models.MakePostResponse(result)
+
+	c.JSON(http.StatusCreated, post)
+
+}
